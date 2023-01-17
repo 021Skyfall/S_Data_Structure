32b74082b4e38b5801831f8d79f822fa0cc7c52a
021Skyfall <021skyfall@gmail.com>
2023-01-17 16:44:30 +0900

    트리 & 그래프 & BST

---
+++ Graph/Adjacency_Matrix.java
@@ -0,0 +1,44 @@
+package Graph;
+
+import java.util.Arrays;
+
+public class Adjacency_Matrix {
+    public static void main(String[] args) {
+        Solution adjMatrix = new Solution();
+        System.out.println(Arrays.deepToString(adjMatrix.getGraph()));
+    }
+
+    static class Solution {
+        private int[][] graph;
+
+        public void setGraph(int size) {
+            graph = new int[size][size];
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    graph[i][j] = 0;
+                }
+            }
+        }
+
+        public int[][] getGraph() {
+            return graph;
+        }
+
+        public void addEdge(int from, int to) {
+            if (from >= graph.length || to >= graph.length) return;
+            graph[from][to] = 1;
+        }
+
+        public boolean hasEdge(int from, int to) {
+            if (from >= graph.length || to >= graph.length) return false;
+            else if (graph[from][to] == 1) return true;
+            else return false;
+        }
+
+        public void removeEdge(int from, int to) {
+            if (from >= graph.length || to >= graph.length) return;
+            graph[from][to] = 0;
+        }
+    }
+}
